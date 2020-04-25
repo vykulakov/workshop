be2e45892df77c93f942264bf01b3e044784b577
Vyacheslav Kulakov <vkulakov@swiftserve.com>
2020-04-25 13:05:22 +0300

    Add config

--- cmd/workshop/main.go
+++ cmd/workshop/main.go
@@ -1,25 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/go-chi/chi"
+	"github.com/ilyakaznacheev/cleanenv"
 
+	"workshop/internal/config"
 	"workshop/internal/handler"
 )
 
 func main() {
+	c := &config.Server{}
+	err := cleanenv.ReadConfig("config.yml", c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	r := chi.NewRouter()
 	h := handler.NewHandler()
 
 	r.Get("/hello", h.Hello)
 
-	log.Print("Start server")
-	err := http.ListenAndServe(":8080", r)
+	addr := fmt.Sprintf("%s:%s", c.Host, c.Port)
+	log.Printf("Start server at: %s", addr)
+
+	err = http.ListenAndServe(addr, r)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	log.Print("Stop server")
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module workshop
 
 go 1.13
 
-require github.com/go-chi/chi v4.1.1+incompatible // indirect
+require (
+	github.com/go-chi/chi v4.1.1+incompatible // indirect
+	github.com/ilyakaznacheev/cleanenv v1.2.2 // indirect
+)
